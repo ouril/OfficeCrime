f201fd0af6f6867e0b11487e8e28b9d74a2cf8c5
ouril <ourilkregg@gmail.com>
2018-03-22 18:37:26 +0300

    added Crime class and layout

--- app/src/main/java/com/example/ouril/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/example/ouril/criminalintent/CrimeFragment.java
@@ -2,14 +2,17 @@ package com.example.ouril.criminalintent;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-
-/**
- * Created by ouril on 22.03.18.
- */
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
 
 public class CrimeFragment extends Fragment {
 
     private Crime mCrime;
+    private EditText mTitleField;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -17,5 +20,29 @@ public class CrimeFragment extends Fragment {
         mCrime = new Crime();
     }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_crime, container, false);
 
+        mTitleField = v.findViewById(R.id.crime_title);
+        mTitleField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mCrime.setTitle(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        return v;
+    }
 }
