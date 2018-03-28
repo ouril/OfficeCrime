9ab1c69131ab6271263a4724a5871a3357c61965
ouril <ourilkregg@gmail.com>
2018-03-28 22:37:08 +0300

    added class CimeLab

---
+++ app/src/main/java/com/example/ouril/criminalintent/CrimeLab.java
@@ -0,0 +1,37 @@
+package com.example.ouril.criminalintent;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Created by ouril on 28.03.18.
+ */
+
+public class CrimeLab {
+    private static CrimeLab sCrimeLab;
+
+    private List<Crime> mCrimes;
+
+    public static CrimeLab get(Context context) {
+        if (sCrimeLab == null) {
+            sCrimeLab = new CrimeLab(context);
+        }
+        return sCrimeLab;
+    }
+
+    private CrimeLab(Context context) {
+        mCrimes = new ArrayList<>();
+    }
+
+    public Crime getCrime(UUID id) {
+        for (Crime crime : mCrimes) {
+            if (crime.getId().equals(id)) {
+                return crime;
+            }
+        }
+        return null;
+    }
+}
